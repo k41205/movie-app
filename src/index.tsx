6eb78e06e0f0c4f4e08d118adeb390959497ebdf
k41205 <giacomocavarretta@gmail.com>
2024-06-23 10:54:11 +0100

    Full review page.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
         <Route path='/movies/favourites' element={<FavouriteMoviesPage />} />
         <Route path='/movies/:id' element={<MoviePage />} />
         <Route path='/movies/upcoming' element={<UpcomingMoviesPage />} />
+        <Route path='/reviews/:id' element={<MovieReviewPage />} />
         <Route path='/' element={<HomePage />} />
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
