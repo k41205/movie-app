34ba3e25d7061de405cfed14535b0467d1a30fb6
k41205 <giacomocavarretta@gmail.com>
2024-06-23 11:23:16 +0100

    Using react-query to manage the browser-based cache.

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <SiteHeader />
-      <Routes>
-        <Route path='/movies/favourites' element={<FavouriteMoviesPage />} />
-        <Route path='/movies/:id' element={<MoviePage />} />
-        <Route path='/movies/upcoming' element={<UpcomingMoviesPage />} />
-        <Route path='/reviews/:id' element={<MovieReviewPage />} />
-        <Route path='/' element={<HomePage />} />
-        <Route path='*' element={<Navigate to='/' />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader />
+        <Routes>
+          <Route path='/movies/favourites' element={<FavoriteMoviesPage />} />
+          <Route path='/movies/:id' element={<MoviePage />} />
+          <Route path='/movies/upcoming' element={<UpcomingMoviesPage />} />
+          <Route path='/reviews/:id' element={<MovieReviewPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
