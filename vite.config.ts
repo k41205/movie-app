5d07dcc55ee9cb51b566e581131ad961f1b64d4b
k41205 <giacomocavarretta@gmail.com>
2024-08-16 10:17:22 +0100

    fix: adjust vite.config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(process.env.PORT) || 3000,
-    host: true,
-  },
+  // comment off just for dev purpose
+  // server: {
+  //   port: parseInt(process.env.PORT) || 3000,
+  //   host: true,
+  // },
 });
